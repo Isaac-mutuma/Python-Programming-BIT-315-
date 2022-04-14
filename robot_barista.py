70cb7c3ef0c18d5ca1ed16f78638614f62d3a89e
Isaac <71688530+Isaac-mutuma@users.noreply.github.com>
2022-04-14 19:09:35 +0300

    Add files via upload
    A robot barista used my coffee shop

---
+++ robot_barista.py
@@ -0,0 +1,20 @@
+
+#Let's build a robot Barista
+
+print ("Welcome to Isaac's coffee!!!!!")
+
+
+
+name = input("What's your name?")
+
+
+
+print("Hello " + name +  " , thank you so much  for coming in today!!")
+
+menu = "Black coffee,  Espereso, Latte, Cappucino"
+
+print(name + ", what would you like from our menu today? Here is what we are serving.\n" + menu)
+
+order = input()
+
+print("Sounds good " + name + ", we'll have that " + order + " ready for you in a moment.")
